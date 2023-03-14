95cd6062e04db367a7501591100b29a41072f0d2
Pradeep Raj <rjpradeep820@gmail.com>
2023-03-14 11:06:42 +0530

    config and req

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -13,7 +13,7 @@ class User(db.Model,UserMixin):
     id = db.Column(db.Integer(),primary_key= True)
     username = db.Column(db.String(24),unique=True,nullable =False)
     email = db.Column(db.String(120), unique= True, nullable =False)
-    password = db.Column(db.String(20), nullable =False)
+    password = db.Column(db.String(120), nullable =False)
     img_file = db.Column(db.String(20),nullable =False , default = 'default.jpeg')
     posts = db.relationship("Post",backref = 'author',lazy = True)
     def __repr__(self):
